71faeb4fb91684733fbc345dc3c3ecd3c917bd64
TheFausap <fausto.saporito@gmail.com>
2022-10-23 13:54:24 +0200

    Initial Commit

---
+++ x160ptap/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  x160ptap
+//
+//  Created by home on 23/10/2022.
+//  Copyright © 2022 GITANES. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
